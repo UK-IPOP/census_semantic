85d3b31eea911c6d7f63d69ed4fb46c6e73c95f2
Nima Seyedtalebi <nseyedtalebi@gmail.com>
2023-10-24 18:27:38 -0400

    Fixed errors in mapping files,changed to in-memory db, changed anything with OS-specific paths, added requirements.txt, and changed how select_vars.py works

--- initdb.py
+++ initdb.py
@@ -1,63 +1,65 @@
 import duckdb
+import pandas as pd
 
 import os
 
 
 def init_db():
-    con = duckdb.connect('census.db')
-    con.sql(""" CREATE OR REPLACE TABLE dp_mm AS
-                SELECT * FROM read_csv('.\\resources\\mm_dp_fixed.csv', columns={'id':'VARCHAR',
-                'mmi':'VARCHAR',
-                'score': 'FLOAT',
-                'umls_preferred_name':'VARCHAR',
-                'cui':'VARCHAR',
-                'semantic_type_list':'VARCHAR',
-                'trigger_information':'VARCHAR',
-                'location':'VARCHAR',
-                'positional_information':'VARCHAR',
-                'mesh_terms':'VARCHAR'},
-                ignore_errors=True)
-    """
-    )
-    con.sql(""" CREATE OR REPLACE TABLE st_mm AS
-                SELECT * FROM read_csv('.\\resources\\mm_st_fixed.csv', columns={'id':'VARCHAR',
-                'mmi':'VARCHAR',
-                'score': 'FLOAT',
-                'umls_preferred_name':'VARCHAR',
-                'cui':'VARCHAR',
-                'semantic_type_list':'VARCHAR',
-                'trigger_information':'VARCHAR',
-                'location':'VARCHAR',
-                'positional_information':'VARCHAR',
-                'mesh_terms':'VARCHAR'},
-                ignore_errors=True)
-    """
-    )
-    con.sql(""" CREATE OR REPLACE TABLE dp_num AS
-        SELECT * FROM read_csv('.\\resources\\num_dp_fixed.csv', columns={'item_id':'VARCHAR',
-        'item_label':'VARCHAR'},
-        ignore_errors=True)
+    columns = {
+        "id": "str",
+        "mmi": "str",
+        "score": "float",
+        "umls_preferred_name": "str",
+        "cui": "str",
+        "semantic_type_list": "str",
+        "trigger_information": "str",
+        "location": "str",
+        "positional_information": "str",
+        "mesh_terms": "str",
+    }
 
-    """
+    mm_dp = pd.read_csv(
+        os.path.join("resources", "mm_dp_fixed.csv"),
+        header=0,
+        dtype=columns,
+        names=columns.keys(),
+        on_bad_lines="skip",
     )
-    con.sql(""" CREATE OR REPLACE TABLE st_num AS
-        SELECT * FROM read_csv('.\\resources\\num_st_fixed.csv', columns={'item_id':'VARCHAR',
-        'item_label':'VARCHAR'},
-        ignore_errors=True)
+    duckdb.sql("CREATE OR REPLACE TABLE dp_mm AS SELECT * FROM mm_dp")
 
-    """
+    mm_st = pd.read_csv(
+        os.path.join("resources", "mm_st_fixed.csv"),
+        header=0,
+        dtype=columns,
+        names=columns.keys(),
+        on_bad_lines="skip",
     )
-    con.sql(""" CREATE OR REPLACE TABLE dp_vars AS
-        SELECT * FROM read_csv_auto('.\\resources\\ACS5_DP.csv', header=True)
+    duckdb.sql("CREATE OR REPLACE TABLE mm_st AS SELECT * FROM mm_st")
 
-    """
+    num_dp = pd.read_csv(
+        os.path.join("resources", "num_dp_fixed.csv"),
+        header=0,
+        names=["item_id", "item_label"],
+        on_bad_lines="skip",
     )
-    con.sql(""" CREATE OR REPLACE TABLE st_vars AS
-        SELECT * FROM read_csv_auto('.\\resources\\ACS5_ST.csv', header=True)
+    duckdb.sql("CREATE OR REPLACE TABLE dp_num AS SELECT * FROM num_dp")
 
-    """
+    num_st = pd.read_csv(
+        os.path.join("resources", "num_st_fixed.csv"),
+        header=0,
+        names=["item_id", "item_label"],
+        on_bad_lines="skip",
     )
-    con.sql(""" CREATE OR REPLACE VIEW dp_vw AS
+    duckdb.sql("CREATE OR REPLACE TABLE st_num AS SELECT * FROM num_st")
+
+    acs5_dp = pd.read_csv(os.path.join("resources", "ACS5_DP.csv"), on_bad_lines="skip")
+    duckdb.sql("CREATE OR REPLACE TABLE dp_vars AS SELECT * from acs5_dp")
+
+    acs5_st = pd.read_csv(os.path.join("resources", "ACS5_ST.csv"), on_bad_lines="skip")
+    duckdb.sql("CREATE OR REPLACE TABLE st_vars AS SELECT * from acs5_st")
+
+    duckdb.sql(
+        """ CREATE OR REPLACE VIEW dp_vw AS
         select dp_num.item_id
         ,dp_num.item_label
         ,dp_mm.id as mm_id
@@ -75,7 +77,8 @@ def init_db():
             on dp_num.item_id = dp_mm.id
     """
     )
-    con.sql(""" CREATE OR REPLACE VIEW dp_vars_to_nums_vw AS
+    duckdb.sql(
+        """ CREATE OR REPLACE VIEW dp_vars_to_nums_vw AS
         select dp_vars.var
         ,dp_vars.label
         ,dp_vars.concept
@@ -86,3 +89,26 @@ def init_db():
             on string_split(dp_vars.label,'!!')[-1] = dp_num.item_label
     """
     )
+    duckdb.sql(
+        """
+         CREATE OR REPLACE MACRO dp_var_lookup(pattern) AS TABLE (
+         WITH item_label_matches AS (
+            SELECT DISTINCT mm_id
+            ,cui
+            ,score
+            ,ROW_NUMBER() OVER (PARTITION BY mm_id ORDER BY score DESC) AS cui_score_rank
+            FROM dp_vw
+            WHERE regexp_matches(item_label, pattern)
+        )
+        select var, item_label
+        from item_label_matches
+        inner join dp_vars_to_nums_vw
+            on item_label_matches.mm_id = dp_vars_to_nums_vw.item_id
+        where regexp_matches(dp_vars_to_nums_vw.var, 'DP\w+\_\d+E')
+        and cui_score_rank=1);
+    """
+    )
+
+
+if __name__ == "__main__":
+    init_db()
--- select_vars.py
+++ select_vars.py
@@ -5,7 +5,8 @@ import us
 from dotenv import load_dotenv
 
 import sys
-from os import environ
+import os
+import json
 
 from initdb import init_db
 
@@ -14,35 +15,19 @@ patterns = [pattern for pattern in sys.argv[1:]]
 if not patterns:
     print("Usage: select_vars.py pattern [ pattern] ...")
     sys.exit(1)
-try:
-    con = duckdb.connect(database='census.db', read_only=True)
-except duckdb.CatalogException:
-    init_db()
-    con = duckdb.connect(database='census.db', read_only=True)
-
-selected_vars = []
+init_db()
+var_label_pairs = []
 for pattern in patterns:
-    con.execute("""
-        WITH item_label_matches AS (
-            SELECT DISTINCT mm_id
-            ,cui
-            ,score
-            ,ROW_NUMBER() OVER (PARTITION BY mm_id ORDER BY score DESC) AS cui_score_rank
-            FROM dp_vw
-            WHERE regexp_matches(item_label, ?)
-        )
-        select dp_vars_to_nums_vw.var
-        from item_label_matches
-        inner join dp_vars_to_nums_vw
-            on item_label_matches.mm_id = dp_vars_to_nums_vw.item_id
-        where regexp_matches(dp_vars_to_nums_vw.var, 'DP\w+\_\d+E')
-        and cui_score_rank=1
-    """, [pattern])
-    selected_vars += con.fetchall()
-selected_vars = [item for sublist in selected_vars for item in sublist ]
+    duckdb.execute("SELECT * FROM dp_var_lookup(?)", [pattern])
+    var_label_pairs += duckdb.fetchall()
+selected_vars = {
+    item[0]: {"label": item[1], "name": item[0]} for item in var_label_pairs
+}
+print(json.dumps(selected_vars))
+"""
 if selected_vars:
     data = censusdata.download(
-     environ['CENSUS_TABLE'],
+     os.environ['CENSUS_TABLE'],
      int(environ['CENSUS_DATA_YEAR']),
      censusdata.censusgeo([("state", environ['STATE_FIPS']), ("county", environ['COUNTY_FIPS'])]),
      selected_vars,
@@ -52,3 +37,4 @@ if selected_vars:
     print(data)
 else:
     print("No matches")
+"""
